76ede945bda32d9349e5b79e68b2c1fe6b06e90a
ArthurGalanti <arthur.galanti@hotmail.com>
2023-06-10 19:33:04 -0300

    Adicionado testes da entidade TodoItem

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,15 @@
+﻿using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests;
+
+[TestClass]
+public class TodoItemTests
+{
+    private readonly TodoItem _validTodo = new TodoItem("Titulo", DateTime.Now, "ArthurGalanti");
+    
+    [TestMethod]
+    public void DadoUmNovoTodoOMesmoNaoPodeSerConcluido()
+    {
+        Assert.AreEqual(_validTodo.Done, false);
+    }
+}
