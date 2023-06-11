a26cd3846548e8ff5489b8402bfad62b1b29ced4
ArthurGalanti <arthur.galanti@hotmail.com>
2023-06-11 00:25:19 -0300

    Adicionado Repositories

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -1,15 +1,21 @@
+using System.Security.Cryptography.Xml;
+using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Handlers;
+using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+// builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("CONNECTION_STRING")));
+builder.Services.AddTransient<ITodoRepository, TodoRepository>();
+builder.Services.AddTransient<TodoHandler, TodoHandler>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
